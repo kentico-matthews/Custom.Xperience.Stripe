bf7521fd8d2a866bd9f4681a34c9382ff83c87ec
Matthew Sandstrom <MatthewS@kentico.com>
2023-01-20 20:50:27 -0500

    Renamed folder, improvements to service reference, bug in return type fixed.

---
+++ Admin/Custom.Xperience.Stripe.Admin/Services/IResolvablePaymentIntentService.cs
@@ -0,0 +1,66 @@
+﻿using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Custom.Xperience.Stripe
+{
+    public interface IResolvablePaymentIntentService
+    {
+        PaymentIntent ApplyCustomerBalance(string id, PaymentIntentApplyCustomerBalanceOptions options = null, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> ApplyCustomerBalanceAsync(string id, PaymentIntentApplyCustomerBalanceOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent Cancel(string id, PaymentIntentCancelOptions options = null, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> CancelAsync(string id, PaymentIntentCancelOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent Capture(string id, PaymentIntentCaptureOptions options = null, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> CaptureAsync(string id, PaymentIntentCaptureOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent Confirm(string id, PaymentIntentConfirmOptions options = null, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> ConfirmAsync(string id, PaymentIntentConfirmOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent Create(PaymentIntentCreateOptions options, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> CreateAsync(PaymentIntentCreateOptions options, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent Get(string id, PaymentIntentGetOptions options = null, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> GetAsync(string id, PaymentIntentGetOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent IncrementAuthorization(string id, PaymentIntentIncrementAuthorizationOptions options = null, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> IncrementAuthorizationAsync(string id, PaymentIntentIncrementAuthorizationOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        StripeList<PaymentIntent> List(PaymentIntentListOptions options = null, RequestOptions requestOptions = null);
+
+        Task<StripeList<PaymentIntent>> ListAsync(PaymentIntentListOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        IEnumerable<PaymentIntent> ListAutoPaging(PaymentIntentListOptions options = null, RequestOptions requestOptions = null);
+
+        IAsyncEnumerable<PaymentIntent> ListAutoPagingAsync(PaymentIntentListOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        StripeSearchResult<PaymentIntent> Search(PaymentIntentSearchOptions options = null, RequestOptions requestOptions = null);
+
+        Task<StripeSearchResult<PaymentIntent>> SearchAsync(PaymentIntentSearchOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        IEnumerable<PaymentIntent> SearchAutoPaging(PaymentIntentSearchOptions options = null, RequestOptions requestOptions = null);
+
+        IAsyncEnumerable<PaymentIntent> SearchAutoPagingAsync(PaymentIntentSearchOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent Update(string id, PaymentIntentUpdateOptions options, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> UpdateAsync(string id, PaymentIntentUpdateOptions options, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+        PaymentIntent VerifyMicrodeposits(string id, PaymentIntentVerifyMicrodepositsOptions options = null, RequestOptions requestOptions = null);
+
+        Task<PaymentIntent> VerifyMicrodepositsAsync(string id, PaymentIntentVerifyMicrodepositsOptions options = null, RequestOptions requestOptions = null, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ Admin/Custom.Xperience.Stripe.Admin/Services/ResolvablePaymentIntentService.cs
@@ -0,0 +1,13 @@
+﻿using Stripe;
+using Custom.Xperience.Stripe;
+using CMS;
+using CMS.Core;
+
+[assembly: RegisterImplementation(typeof(IResolvablePaymentIntentService), typeof(ResolvablePaymentIntentService), Priority = RegistrationPriority.Default)]
+namespace Custom.Xperience.Stripe
+{
+    internal class ResolvablePaymentIntentService : PaymentIntentService, IResolvablePaymentIntentService
+    {
+
+    }
+}
