4cbc81af2354465991dbe36b8ab90ad014103329
Matthew Sandstrom <MatthewS@kentico.com>
2023-01-30 10:56:08 -0500

    Added class to hold constants for access in multipel other places

---
+++ Admin/Custom.Xperience.Stripe.Admin/Constants/XperienceStripeConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Custom.Xperience.Stripe
+{
+    public static class XperienceStripeConstants
+    {
+        public const string PAYMENT_INTENT_ID_KEY = "StripePaymentIntentID";
+
+        public const string CHECKOUT_ID_KEY = "StripeCheckoutID";
+    }
+}
